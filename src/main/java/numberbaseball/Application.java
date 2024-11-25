733f502b0ff2adf23b086d479a42b7ad36499baf
TJOEUNIS <minsung712@yahoo.co.kr>
2024-11-25 17:35:28 +0900

    ball 갯수 추가

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -24,27 +24,39 @@ public class Application {
             System.out.print("세 번째 숫자를 입력하세요: ");
             int user3 = scanner.nextInt();
 
-            int[] userGuess = {user1, user2, user3};
 
             // TODO: strike 개수를 계산하세요
             int strike = 0;
 
-            for (int i = 0; i < 3; i++) {
-                if (com1 == userGuess[i]) {
+
+                if (com1 == user1) {
                     strike++;
                 }
-                if (com2 == userGuess[i]) {
+                if (com2 == user2) {
                     strike++;
                 }
-                if (com3 == userGuess[i]) {
+                if (com3 == user3) {
                     strike++;
                 }
+                // TODO: ball 갯수를 계산
+            int ball = 0;
+            if (com2 == user1  || com3 == user1 ) {
+                ball++;
+            }
+            if ( com1 == user2 || com3 == user2 ) {
+                ball++;
+            }
+            if (com1 == user3  || com2 == user3 ) {
+                ball++;
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.println("아웃");
+            } else {
+                System.out.println(strike + " 스트라이크, " + ball + " 볼");
             }
             // TODO: 결과를 출력하세요 (예: "1 스트라이크")
             if (strike == 0) {
                 System.out.println("아웃");
-            } else {
-                System.out.println(strike + " 스트라이크");
             }
 
             // TODO: 3 스트라이크인 경우 게임을 끝내세요
